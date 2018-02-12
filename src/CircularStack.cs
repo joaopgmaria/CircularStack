f70eef9f479d9ee61e027ee9bf7a4d06fabd3fcd
João Maria <joao.maria@sky.uk>
2018-02-12 15:50:01 +0000

    Initial Code (WIP)

---
+++ src/CircularStack.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CircularStack
+{
+    public class CircularStack : CircularStack<object>
+    {
+        public CircularStack() : base()
+        {
+        }
+
+        public CircularStack(int capacity) : base(capacity)
+        {
+        }
+
+        public CircularStack(IEnumerable<object> items) : base(items)
+        {
+        }
+
+        public CircularStack(IEnumerable<object> items, int capacity) : base(items, capacity)
+        {
+        }
+
+        public CircularStack(IEnumerable<object> items, int capacity, int last) : base(items, capacity, last)
+        {
+        }
+    }
+
+    public class CircularStack<T> : ICollection, IEnumerable, ICloneable
+    {
+        public static int DefaultCapacity => 10;
+
+        private T[] _space;
+        private int _last = -1;
+        private object _syncRoot = new object();
+
+        public int Capacity => _space.Length;
+        public int Count => _last + 1;
+        public bool IsFull => Count == Capacity;
+        public bool IsEmpty => Count == 0;
+        public bool IsSynchronized => false;
+        public object SyncRoot => _syncRoot;
+
+        public CircularStack()
+        {
+            _space = new T[DefaultCapacity];
+            _last = -1;
+        }
+
+        public CircularStack(int capacity)
+        {
+            _space = new T[capacity];
+            _last = -1;
+        }
+
+        public CircularStack(IEnumerable<T> items)
+        {
+            int count = items.Count();
+
+            _space = new T[count];
+
+            items.ToList()
+                 .ForEach(i => Push(i));
+
+            _last = count - 1;
+        }
+
+        public CircularStack(IEnumerable<T> items, int capacity)
+        {
+            int count = items.Count();
+
+            _space = new T[capacity];
+
+            items.ToList()
+                 .ForEach(i => Push(i));
+
+            _last = count - 1;
+        }
+
+        public CircularStack(IEnumerable<T> items, int capacity, int last) : this(items, capacity)
+        {
+            _last = last;
+        }
+
+        /// <summary>  
+        /// Push Item in Front  
+        /// </summary>  
+        /// <param name="item"></param>  
+        public void Push(T item)
+        {
+            if (Count == Capacity)
+            {
+                _last--;
+            }
+
+            if (Count < Capacity)
+            {
+                for (int i = _last; i >= 0; i--)
+                {
+                    _space[i + 1] = _space[i];
+                }
+                
+                _space[0] = item;
+                _last++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is Full");
+            }
+
+        }
+
+        /// <summary>  
+        /// Pop Item In Front  
+        /// </summary>  
+        /// <returns></returns>  
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+
+            var popedItem = _space[0];
+
+            for (int i = _last; i > 0; i--)
+            {
+                _space[i - 1] = _space[i];
+            }
+
+            _space[_last] = default(T);
+            _last--;
+
+            return popedItem;
+        }
+
+        /// <summary>  
+        /// Peek Item In Front  
+        /// </summary>  
+        /// <returns></returns>  
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+
+            return _space[0];
+        }
+
+        public IEnumerator GetEnumerator() => _space.GetEnumerator();
+
+        public void CopyTo(Array array, int index)
+        {
+            foreach (T i in _space)
+            {
+                array.SetValue(i, index);
+                index = index + 1;
+            }
+        }
+
+        public object Clone()
+        {
+            var items = _space.ToList();
+
+            return new CircularStack<T>(items, Capacity, _last);
+        }
+    }
+}
---
+++ tests/CircularStackTests.cs
@@ -0,0 +1,265 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CircularStack
+{
+    [TestClass]
+    public class CircularStackTests
+    {
+        [TestMethod]
+        public void CircularStack_Should_BeInitializedWithDefaultCapacity()
+        {
+            var obj = new CircularStack();
+            Assert.AreEqual(CircularStack.DefaultCapacity, obj.Capacity);
+        }
+
+        [TestMethod]
+        public void CircularStack_Should_BeInitializedWithProvidedCapacity()
+        {
+            int capacity = 100;
+
+            var obj = new CircularStack(capacity);
+            Assert.AreEqual(capacity, obj.Capacity);
+        }
+
+        [TestMethod]
+        public void CircularStack_Should_BeInitializedWithCollection()
+        {
+            var items = new List<string> { "a", "b" };
+            
+            var obj = new CircularStack(items);
+
+            Assert.AreEqual(2, obj.Count);
+            Assert.AreEqual(items.Count, obj.Capacity);
+            Assert.AreSame(items[1], obj.Peek());
+        }
+
+        [TestMethod]
+        public void CircularStack_Should_BeInitializedWithCollectionAndCapacity()
+        {
+            var items = new List<string> { "a", "b" };
+
+            var obj = new CircularStack(items, 4);
+
+            Assert.AreEqual(2, obj.Count);
+            Assert.AreEqual(4, obj.Capacity);
+            Assert.AreSame(items[1], obj.Peek());
+        }
+
+        [TestMethod]
+        public void Count_Should_ReturnItemCount()
+        {
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            Assert.AreEqual(2, obj.Count);
+        }
+
+        [TestMethod]
+        public void Count_Should_ReturnZero_When_StackIsEmpty()
+        {
+            var obj = new CircularStack();
+
+            Assert.AreEqual(0, obj.Count);
+        }
+
+        [TestMethod]
+        public void IsFull_Should_ReturnTrue_WhenStackIsFull()
+        {
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            Assert.IsTrue(obj.IsFull);
+        }
+
+        [TestMethod]
+        public void Push_Should_AddItem_WhenStackIsEmpty()
+        {
+            var obj = new CircularStack();
+
+            obj.Push(1);
+
+            Assert.AreEqual(1, obj.Count);
+        }
+
+        [TestMethod]
+        public void Push_Should_AddItem()
+        {
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr, 4);
+
+            obj.Push("c");
+
+            Assert.AreEqual(3, obj.Count);
+        }
+        
+        [TestMethod]
+        public void Push_Should_AddItem_When_ThereIsOnlyOneSpaceLeft()
+        {
+            var obj = new CircularStack(2);
+
+            obj.Push(1);
+            obj.Push(2);
+
+            Assert.AreEqual(2, obj.Count);
+        }
+
+        [TestMethod]
+        public void Push_Should_DropLastItem_When_StackIsFull()
+        {
+            var expected = "b";
+            var arr = new List<string> { "a", expected };
+            var obj = new CircularStack(arr);
+            
+            obj.Push("c");
+
+            obj.Pop(); //c
+            var actual = obj.Pop();
+
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void Peek_Should_ReturnLastItem()
+        {
+            var expected = "b";
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            var actual = obj.Peek();
+
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void Peek_ShouldNot_RemoveItem()
+        {
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            obj.Peek();
+
+            Assert.AreEqual(2, obj.Count);
+        }
+
+        [TestMethod]
+        public void Pop_Should_ReturnItem()
+        {
+            var expected = "b";
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            var actual = obj.Pop();
+
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pop_Should_ReturnItem_When_ThereIsOnlyOne()
+        {
+            var expected = "a";
+            var arr = new List<string> { "a" };
+            var obj = new CircularStack(arr);
+
+            var actual = obj.Pop();
+
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pop_Should_RemoveItem()
+        {
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            obj.Pop();
+
+            Assert.AreEqual(1, obj.Count);
+        }
+
+        [TestMethod]
+        public void Pop_Should_RemoveItem_When_ThereIsOnlyOne()
+        {
+            var arr = new List<string> { "a" };
+            var obj = new CircularStack(arr);
+
+            obj.Pop();
+
+            Assert.AreEqual(0, obj.Count);
+        }
+
+        [TestMethod]
+        public void Pop_Should_RespectCapacity()
+        {
+            var arr = new List<object> { new { } };
+            var obj = new CircularStack(arr);
+
+            obj.Pop();
+
+            Assert.ThrowsException<InvalidOperationException>(() => obj.Pop());
+        }
+
+        [TestMethod]
+        public void Push_ShouldNot_ChangeCapacity()
+        {
+            var obj = new CircularStack();
+
+            obj.Push(1);
+
+            Assert.AreEqual(CircularStack.DefaultCapacity, obj.Capacity);
+        }
+
+        [TestMethod]
+        public void Pop_ShouldNot_ChangeCapacity()
+        {
+            var arr = new List<object> { new { }, new { } };
+            var obj = new CircularStack(arr);
+
+            obj.Pop();
+
+            Assert.AreEqual(2, obj.Capacity);
+        }
+
+        [TestMethod]
+        public void Push_Should_WorkAfterPop()
+        {
+            var expected = "c";
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr);
+
+            obj.Pop();
+            obj.Push(expected);
+
+            Assert.AreSame(expected, obj.Peek());
+        }
+
+        [TestMethod]
+        public void Pop_Should_WorkAfterPush()
+        {
+            var expected = "a";
+            var obj = new CircularStack();
+            
+            obj.Push(expected);
+            var actual = obj.Pop();
+
+            Assert.AreSame(expected, actual);
+        }
+
+        [TestMethod]
+        public void Clone_Should_Work()
+        {
+            //Create stack with capacity 4 and 2 items
+            var arr = new List<string> { "a", "b" };
+            var obj = new CircularStack(arr, 4);
+
+            var actual = obj.Clone() as CircularStack<object>;
+
+            Assert.AreEqual(obj.Capacity, actual.Capacity);
+            Assert.AreEqual(obj.Count, actual.Count);
+            Assert.AreEqual(obj.IsEmpty, actual.IsEmpty);
+            Assert.AreEqual(obj.IsFull, actual.IsFull);
+            Assert.AreEqual(obj.IsSynchronized, actual.IsSynchronized);
+        }
+    }
+}
